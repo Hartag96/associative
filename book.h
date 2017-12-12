2449c2f07d9338616a88d7c6201710f93fccc0b1
Linux CTI <hartag96@gmail.com>
2017-12-12 14:44:33 +0100

    nie dziala nadal

---
+++ book.h
@@ -0,0 +1,18 @@
+#ifndef __BOOK_H__
+#define __BOOK_H__
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <malloc.h>
+#include <iostream>
+using namespace std;
+
+class book
+{
+	typedef unsigned int ID; 							//Identification number of Employee
+	map_template<ID,Employee> Database;					//Database of employees
+
+}
+
+#endif /* __BOOK_H__ */
---
+++ testbook.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+#include "Employee.h"			//defines class Employee
+#include "map_template.h"		//defines template map_template<Key,Value>
+#include "book.h"
+
+int main(void)
+{
+
+}
